11cc942294f44aa35d082f05f16b26aed751f00a
Fozat Kardouh <fozatkardouh@gmail.com>
2018-04-24 10:25:45 +0200

    added empty project

---
+++ src/main/java/at/refugeescode/mp19_pirates_of_the_codebean/Mp19PiratesOfTheCodebeanApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.mp19_pirates_of_the_codebean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp19PiratesOfTheCodebeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp19PiratesOfTheCodebeanApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/mp19_pirates_of_the_codebean/Mp19PiratesOfTheCodebeanApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.mp19_pirates_of_the_codebean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Mp19PiratesOfTheCodebeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
